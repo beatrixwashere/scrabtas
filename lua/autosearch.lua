c07b89966b409dc93ef4ee45871f4ff5c3204083
thequeenbea <127708031+thequeenbea@users.noreply.github.com>
2023-08-13 17:06:25 -0500

    update autosearch to v.04.00
    - saves the addresses into a .wch file, making them much easier to import
    - put spd above pos in results
    - removed obsolete failsafe

--- lua/autosearch.lua
+++ lua/autosearch.lua
@@ -1,17 +1,19 @@
---- autosearch v.03.03 --- by queenbea ---
+--- autosearch v.04.00 --- by queenbea ---
 
 --- usage instructions ---
--- 1) go to tools > lua > execute lua script and run this script
--- 2) go to tools > lua > lua console to see the output
--- 3) playback 00_(start)_poet.ltm
--- 4) unpause and wait
--- 5) insert the results into ram watch
+-- 1) go to tools > lua console
+-- 2) go to script > add script file and open this script
+-- 3) playback x_(start)_poet.ltm
+-- 4) switch to recording, unpause, and wait
+-- 5) go to tools > ram watch
+-- 5) use load watches and open output.wch, or add the results into ram watch manually
 
 --- debugging ---
 -- 1a) manually search for the xpix address and check if it's out of range (range is lowerbound to upperbound)
 -- 1b) subtract the xspd address from the xpix address and check if it's in range (range is spdlbound to spdubound)
 -- 2) if so, report it in the tasing thread and i'll include it in the range
 -- 3) if the address is within the range, send the full console log and address in the tasing thread and i'll try my best to fix it
+-- 4) if output.wch doesn't save or import correctly, send the error or output file and i'll try my best to fix it
 
 -- setup variables
 sframe, spix = 1292, 266
@@ -21,7 +23,7 @@ xpos, ypos, xpix, ypix, xspd, yspd = 0x0
 searched = false
 function onStartup()
     -- reset variables
-    print("-- autosearch v.03.00 -- by queenbea --")
+    print("-- autosearch v.04.00 -- by queenbea --")
     sframe, spix = 1292, 266
     lowerbound, upperbound = 0x364fb5e8, 0x38261cb8
     spdlbound, spdubound = 0x142ff0, 0x2b4250
@@ -62,11 +64,8 @@ function onFrame()
             for i=1,addresslength do
                 currentaddress = addresslist[i]
                 if memory.readf(currentaddress) == spix+1 then
-                    print("xpix address found! [source: addresslist]")
+                    print("xpix address found!")
                     xpix = currentaddress
-                elseif memory.readf(currentaddress+0xb80) == 267 then
-                    print("xpix address found! [source: failsafe]")
-                    xpix = currentaddress+0xb80
                 end
             end
             -- offset address
@@ -136,8 +135,14 @@ function onFrame()
             -- offset address
             yspd = xspd+0x4
             -- print all addresses
+            print("-- results --")
+            if xspd ~= 0x0 then
+                print("xspd: "..string.format("%x",xspd))
+                print("yspd: "..string.format("%x",yspd))
+            else
+                print("error: xspd address not found")
+            end
             if xpix ~= 0x0 then
-                print("-- results --")
                 print("xpos: "..string.format("%x",xpos))
                 print("ypos: "..string.format("%x",ypos))
                 print("xpix: "..string.format("%x",xpix))
@@ -145,13 +150,18 @@ function onFrame()
             else
                 print("error: xpix address not found")
             end
-            if xspd ~= 0x0 then
-                print("xspd: "..string.format("%x",xspd))
-                print("yspd: "..string.format("%x",yspd))
-            else
-                print("error: xspd address not found")
-            end
             searched = true;
+            -- save ramwatch file
+            print("attempting to save ramwatch file...")
+            local file,err = io.open("output.wch",'w')
+            if file then
+                file:write("[watches]\n1\\address="..xspd.."\n1\\hex=false\n1\\isPointer=false\n1\\label=xspd\n1\\type=8\n2\\address="..yspd.."\n2\\hex=false\n2\\isPointer=false\n2\\label=yspd\n2\\type=8\n3\\address="..xpos.."\n3\\hex=false\n3\\isPointer=false\n3\\label=xpos\n3\\type=8\n4\\address="..ypos.."\n4\\hex=false\n4\\isPointer=false\n4\\label=ypos\n4\\type=8\n5\\address="..xpix.."\n5\\hex=false\n5\\isPointer=false\n5\\label=xpix\n5\\type=8\n6\\address="..ypix.."\n6\\hex=false\n6\\isPointer=false\n6\\label=ypix\n6\\type=8\nsize=6")
+                file:close()
+                print("file saved successfully! :)")
+            else
+                print("error: ", err)
+                print("file not saved :(")
+            end
         end
     end
 end
